60352a74fd0cf546c7a2eb52d50fdee31ff66f75
Mike Mather <michael_mather@hotmail.com>
2023-09-14 08:02:23 -0230

    Add sitemap

---
+++ kvpear-frontend/next-sitemap.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https:kvpear.dev',
+  generateRobotsTxt: true,
+  exclude: [
+    '/account',
+    '/api-keys',
+    '/app',
+    '/buckets',
+    '/usage'
+  ]
+}
