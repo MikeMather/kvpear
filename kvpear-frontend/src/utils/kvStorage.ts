ee5513cb6a14b8d4a6b896c4e733605017eca215
Mike Mather <michael_mather@hotmail.com>
2023-09-15 07:39:14 -0230

    Fix user storage test

--- kvpear-frontend/src/utils/kvStorage.ts
+++ kvpear-frontend/src/utils/kvStorage.ts
@@ -4,7 +4,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_KV_API_URL;
 const API_KEY = process.env.NEXT_PUBLIC_KVPEAR_API_KEY;
 
 export const storeKv = async (bucket: string, key: string, value: any) => {
-  const url = `${BASE_URL}/v1/kvs/${bucket}/${key}`;
+  const url = `${BASE_URL}/kvs/${bucket}/${key}`;
   const headers = {
     'Content-Type': 'text/plain',
     'x-api-key': API_KEY as string,
